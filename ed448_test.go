08d099b07dbcfc0ac17ae9f457fe041ae801f594
Daisuke Maki <lestrrat+github@gmail.com>
2026-04-02 17:07:42 +0900

    overhaul to dsig extension from jwx extension

--- ed448_test.go
+++ ed448_test.go
@@ -1,16 +1,13 @@
 package ed448_test
 
 import (
-	"encoding/json"
 	"testing"
 
 	"github.com/cloudflare/circl/sign/ed448"
-	"github.com/lestrrat-go/jwx/v3/jwa"
-	"github.com/lestrrat-go/jwx/v3/jwk"
-	"github.com/lestrrat-go/jwx/v3/jws"
+	"github.com/lestrrat-go/dsig"
 	"github.com/stretchr/testify/require"
 
-	_ "github.com/lestrrat-go/jwx-circl-ed448"
+	_ "github.com/lestrrat-go/dsig-circl-ed448"
 )
 
 func TestEd448SignVerifyRoundtrip(t *testing.T) {
@@ -21,95 +18,39 @@ func TestEd448SignVerifyRoundtrip(t *testing.T) {
 
 	payload := []byte("Ed448 roundtrip test")
 
-	t.Run("raw keys", func(t *testing.T) {
-		t.Parallel()
-		signed, err := jws.Sign(payload, jws.WithKey(jwa.EdDSAEd448(), priv))
-		require.NoError(t, err, "signing should succeed")
+	signed, err := dsig.Sign(priv, "Ed448", payload, nil)
+	require.NoError(t, err, "signing should succeed")
 
-		verified, err := jws.Verify(signed, jws.WithKey(jwa.EdDSAEd448(), pub))
-		require.NoError(t, err, "verifying should succeed")
-		require.Equal(t, payload, verified)
-	})
-
-	t.Run("JWK keys", func(t *testing.T) {
-		t.Parallel()
-		jwkPriv, err := jwk.Import(priv)
-		require.NoError(t, err, "importing Ed448 private key should succeed")
-
-		jwkPub, err := jwk.Import(pub)
-		require.NoError(t, err, "importing Ed448 public key should succeed")
-
-		signed, err := jws.Sign(payload, jws.WithKey(jwa.EdDSAEd448(), jwkPriv))
-		require.NoError(t, err, "signing with JWK key should succeed")
-
-		verified, err := jws.Verify(signed, jws.WithKey(jwa.EdDSAEd448(), jwkPub))
-		require.NoError(t, err, "verifying with JWK key should succeed")
-		require.Equal(t, payload, verified)
-	})
-
-	t.Run("alg header is Ed448", func(t *testing.T) {
-		t.Parallel()
-		signed, err := jws.Sign(payload, jws.WithKey(jwa.EdDSAEd448(), priv))
-		require.NoError(t, err)
-
-		msg, err := jws.Parse(signed)
-		require.NoError(t, err)
-		sigs := msg.Signatures()
-		require.Len(t, sigs, 1)
-		alg, ok := sigs[0].ProtectedHeaders().Algorithm()
-		require.True(t, ok)
-		require.Equal(t, jwa.EdDSAEd448(), alg)
-	})
+	err = dsig.Verify(pub, "Ed448", payload, signed)
+	require.NoError(t, err, "verifying should succeed")
 }
 
-func TestEd448JWKRoundtrip(t *testing.T) {
+func TestEd448VerifyWrongKey(t *testing.T) {
 	t.Parallel()
 
-	pub, priv, err := ed448.GenerateKey(nil)
+	_, priv, err := ed448.GenerateKey(nil)
 	require.NoError(t, err)
 
-	t.Run("public key import/export", func(t *testing.T) {
-		t.Parallel()
-		jwkKey, err := jwk.Import(pub)
-		require.NoError(t, err)
+	otherPub, _, err := ed448.GenerateKey(nil)
+	require.NoError(t, err)
 
-		crv, ok := jwkKey.(jwk.OKPPublicKey).Crv()
-		require.True(t, ok)
-		require.Equal(t, jwa.Ed448(), crv)
+	payload := []byte("wrong key test")
 
-		var exported ed448.PublicKey
-		require.NoError(t, jwk.Export(jwkKey, &exported))
-		require.Equal(t, pub, exported)
-	})
+	signed, err := dsig.Sign(priv, "Ed448", payload, nil)
+	require.NoError(t, err)
 
-	t.Run("private key import/export", func(t *testing.T) {
-		t.Parallel()
-		jwkKey, err := jwk.Import(priv)
-		require.NoError(t, err)
-
-		crv, ok := jwkKey.(jwk.OKPPrivateKey).Crv()
-		require.True(t, ok)
-		require.Equal(t, jwa.Ed448(), crv)
-
-		var exported ed448.PrivateKey
-		require.NoError(t, jwk.Export(jwkKey, &exported))
-		require.Equal(t, priv.Seed(), exported.Seed())
-	})
-
-	t.Run("JSON roundtrip", func(t *testing.T) {
-		t.Parallel()
-		jwkKey, err := jwk.Import(priv)
-		require.NoError(t, err)
-
-		buf, err := json.Marshal(jwkKey)
-		require.NoError(t, err)
-
-		parsedKey, err := jwk.ParseKey(buf)
-		require.NoError(t, err)
-
-		var exported ed448.PrivateKey
-		require.NoError(t, jwk.Export(parsedKey, &exported))
-		require.Equal(t, priv.Seed(), exported.Seed())
-	})
+	err = dsig.Verify(otherPub, "Ed448", payload, signed)
+	require.Error(t, err, "verifying with wrong key should fail")
 }
 
+func TestEd448InvalidKeyType(t *testing.T) {
+	t.Parallel()
+
+	payload := []byte("invalid key test")
+
+	_, err := dsig.Sign("not a key", "Ed448", payload, nil)
+	require.Error(t, err, "signing with invalid key type should fail")
+
+	err = dsig.Verify("not a key", "Ed448", payload, []byte("fake sig"))
+	require.Error(t, err, "verifying with invalid key type should fail")
+}
--- go.mod
+++ go.mod
@@ -1,27 +1,17 @@
-module github.com/lestrrat-go/jwx-circl-ed448
+module github.com/lestrrat-go/dsig-circl-ed448
 
 go 1.25.0
 
 require (
 	github.com/cloudflare/circl v1.6.3
-	github.com/lestrrat-go/jwx/v3 v3.0.14-0.20260401135236-89c1d7848e6e
+	github.com/lestrrat-go/dsig v1.0.1-0.20260402074825-c0d43bb43299
 	github.com/stretchr/testify v1.11.1
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.4.1 // indirect
-	github.com/goccy/go-json v0.10.6 // indirect
-	github.com/lestrrat-go/blackmagic v1.0.4 // indirect
-	github.com/lestrrat-go/dsig v1.0.0 // indirect
-	github.com/lestrrat-go/dsig-secp256k1 v1.0.0 // indirect
-	github.com/lestrrat-go/httpcc v1.0.1 // indirect
-	github.com/lestrrat-go/httprc/v3 v3.0.5 // indirect
-	github.com/lestrrat-go/option/v2 v2.0.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/segmentio/asm v1.2.1 // indirect
-	github.com/valyala/fastjson v1.6.10 // indirect
-	golang.org/x/crypto v0.49.0 // indirect
-	golang.org/x/sys v0.42.0 // indirect
+	golang.org/x/crypto v0.30.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
